fc7f1dac910c9557998cebf9aa0d259470dcbe9e
Samson Akanbi <samson.akanbi@outlook.com>
2022-12-07 20:58:29 +0100

    Created the CurrencyExchange repository and controller.

--- src/main/java/com/samseenpractice/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/samseenpractice/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -12,12 +12,22 @@ import java.math.BigDecimal;
 public class CurrencyExchangeController {
 
     @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @Autowired
     private Environment environment;
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to) {
-        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+
+        System.out.println("Now in currency exchange.");
+
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " +from+ " to "+to);
+        }
 
         String port = environment.getProperty("local.server.port");
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+ values(10001, 'USD', 'NGN', 650,'');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+ values(10002, 'USD', 'GBP', 2,'');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+ values(10003, 'USD', 'AUD', 75,'');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+ values(10004, 'USD', 'INR', 65,'');
